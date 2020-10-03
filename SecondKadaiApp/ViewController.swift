bb91c26016e799e4c0f9e0a8433821a5818787b9
mari.saito <mari.saito.tec@gmail.com>
2020-10-04 01:01:06 +0900

    コメントを追加

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -22,11 +22,13 @@ class ViewController: UIViewController,UITextFieldDelegate {
 
     }
     
+    // キーボード入力の決定ボタンで、入力用キーボードを非表示にする
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
 
+    // UITextField以外の部分をタッチした場合に非表示
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
        // TextFieldから文字を取得
         textFieldString = textField.text!
@@ -37,7 +39,7 @@ class ViewController: UIViewController,UITextFieldDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のHelloViewControllerを取得する
         let helloViewController:HelloViewController = segue.destination as! HelloViewController
-        // 遷移先のResultViewControllerで宣言しているNameに値を代入して渡す
+        // 遷移先のhelloViewControllerで宣言しているNameに値を代入して渡す
         helloViewController.Name = textFieldString
         
     }
